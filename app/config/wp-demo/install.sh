b71b372ecfbf7e8efa096887c812ab384a8b4149
Kevin Cristiano <kcristiano@kcristiano.com>
2020-07-03 08:03:25 -0400

    add disable health check plugin
    Signed-off-by: Kevin Cristiano <kcristiano@kcristiano.com>

--- app/config/wp-demo/install.sh
+++ app/config/wp-demo/install.sh
@@ -63,6 +63,7 @@ wp plugin install civicrm-admin-utilities
 wp plugin install gutenberg
 wp plugin install classic-editor --activate
 wp plugin activate civicrm-gutenberg-blocks
+wp plugin activate disable-health-check
 
 civicrm_apply_demo_defaults
 cv ev 'if(is_callable(array("CRM_Core_BAO_CMSUser","synchronize"))){CRM_Core_BAO_CMSUser::synchronize(FALSE);}else{CRM_Utils_System::synchronizeUsers();}'
